626407b31058ea0139dbbbe73d1b0bb9cd934828
rjsah09 <rjsah09@naver.com>
2024-08-16 10:36:43 +0900

    모든 글 조회

--- app/src/main/java/com/example/demo/domain/subways/SubwayRepository.java
+++ app/src/main/java/com/example/demo/domain/subways/SubwayRepository.java
@@ -3,14 +3,19 @@ package com.example.demo.domain.subways;
 import com.example.demo.interfaces.controller.subway.dto.SubwayAllInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SubwayRepository extends JpaRepository<Subway, Long> {
 
     @Query("select new com.example.demo.interfaces.controller.subway.dto.SubwayAllInfo(s.id, s.name, s.color) from Subway s")
     List<SubwayAllInfo> getSubways();
+
+    @Query("select s.name from Subway s where s.id = :id")
+    Optional<String> getNameById(@Param("id") long id);
 
 }
--- app/src/main/java/com/example/demo/interfaces/controller/subway/SubwayController.java
+++ app/src/main/java/com/example/demo/interfaces/controller/subway/SubwayController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,11 @@ public class SubwayController {
     @GetMapping("/list")
     public ResponseEntity<List<SubwayAllInfo>> getSubways() {
         return new ResponseEntity<List<SubwayAllInfo>>(subwayRepository.getSubways(), HttpStatus.OK);
+    }
+
+    @GetMapping("/name/{subwayId}")
+    public String getSubway(@PathVariable long subwayId) {
+        return subwayRepository.getNameById(subwayId).isPresent() ? subwayRepository.getNameById(subwayId).get() : "";
     }
 
 }
